a6304ba179df9c456542833c5831fc65e5361940
Wouna <wouna@love-you.xyz>
2020-07-01 12:16:49 +0200

    Update link to work.

--- src/love-you.js
+++ src/love-you.js
@@ -2,12 +2,11 @@ const snekfetch = require('snekfetch');
 
 const Module = {
 
-  async images(endpoint, token) {
+  async nsfw(endpoint) {
     return new Promise(async(resolve, reject) => {
-      if (!endpoint) return reject('[Love-You] Error, no endpoint specified');
       if (!token) return reject('[Love-You] Error, no token specified');
       try {
-        await snekfetch.get(`https://love-you.xyz/api/v1/${endpoint}?token=${token}`).then(res => {
+        await snekfetch.get(`https://love-you.xyz/api/v2/${endpoint}`).then(res => {
           if (res.error) reject(res.error);
           resolve(res.body.url)
         }).catch(err => {
