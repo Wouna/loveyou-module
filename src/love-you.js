c6212dad61a91c5a04a341f8fdfec9048303579a
Stijn Bannink <27051051+stijnb1234@users.noreply.github.com>
2020-07-18 20:30:17 +0200

    Beautified code

--- src/love-you.js
+++ src/love-you.js
@@ -2,26 +2,24 @@ const axios = require('axios');
 
 const Module = {
 
-  async nsfw(endpoint) {
-    return new Promise(async(resolve, reject) => {
-      if (!endpoint) return reject('[Love-You] Error, no endpoint specified');
-      try {
-        await axios({
-            method: 'get',
-            url: `https://love-you.xyz/api/v2/${endpoint}`,
-            responseType: 'json'
-          })
-            .then(function (response) {
-              resolve(response.data.url);
-            })
-            .catch(err => {
-          return reject(err);
+    async nsfw(endpoint) {
+        return new Promise(async (resolve, reject) => {
+            if (!endpoint) return reject('[Love-You] Error, no endpoint specified');
+            try {
+                await axios({
+                    method: 'get',
+                    url: `https://love-you.xyz/api/v2/${endpoint}`,
+                    responseType: 'json'
+                }).then(function(response) {
+                    resolve(response.data.url);
+                }).catch(err => {
+                    return reject(err);
+                });
+            } catch (e) {
+                return reject(err.message)
+            };
         });
-      } catch (e) {
-        return reject(err.message)
-      };
-    });
-  }
+    }
 }
 
 module.exports = Module;
